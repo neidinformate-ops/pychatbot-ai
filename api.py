bbcb80863f810c9bedeb6b2de8d89821ee276d5f
neidinformate-ops <neidinformate@gmail.com>
2026-05-24 11:22:37 +0200

    fix syntax error1

--- api.py
+++ api.py
@@ -64,7 +64,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://twoj-frontend.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
